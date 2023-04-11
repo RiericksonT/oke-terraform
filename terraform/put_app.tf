2064067ed6aaea1292a5401be09dea45360ceecd
Kaique Rierickson Torres Silva <53712152+RiericksonT@users.noreply.github.com>
2023-04-11 12:20:00 -0300

    Delete terraform directory

--- terraform/put_app.tf
+++
@@ -1,8 +0,0 @@
-resource "null_resource" "app" {
-  depends_on = [
-    oci_containerengine_cluster.oke-cluster
-  ]
-  provisioner "local-exec" {
-    command = "sudo kubectl apply -R -f ~/app/microservice-app-example"
-  }
-}
